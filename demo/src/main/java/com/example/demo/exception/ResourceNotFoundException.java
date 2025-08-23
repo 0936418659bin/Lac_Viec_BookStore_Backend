4989c86cf39473fba72620e22588d08876ce4546
Minh Hiếu <minhhieu051980@gmail.com>
2025-08-23 12:30:42 +0700

    feat: add modelmapper and swagger dependencies

--- demo/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ demo/src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -5,9 +5,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
+    
+    public ResourceNotFoundException() {
+        super();
+    }
+    
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+    
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
